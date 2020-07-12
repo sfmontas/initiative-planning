3eaad32d53a26ce6d1eb9c56a21793577f1dc14e
Francisco <francisco_montas@ultimatesoftware.com>
2020-07-12 10:57:28 -0700

    Added initial endpoint with docker file and docker compose file to run. Also added API test.

---
+++ api/api/api_test.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def test_post_headers_body_json():
+    url = 'http://localhost:8000/'
+    # Additional headers.
+    headers = {'Content-Type': 'application/json' }
+
+    # Body
+    # convert dict to json by json.dumps() for body data.
+    resp = requests.get(url)
+    # Validate response headers and body contents, e.g. status code.
+    assert resp.status_code == 200
+    resp_body = resp.json()
+    assert resp_body['message'] == "Hello World!"
