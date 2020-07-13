7eb341e11d18edfd0916be9556ecf70fe23c64d5
Francisco <francisco_montas@ultimatesoftware.com>
2020-07-12 22:10:13 -0700

    Added endpoints to create, update and view an initiative. Also added endpoint for viewing event stream for initiatives. Lastly added some tests for each endpoint happy path.

--- api/api/api_test.py
+++ api/api/api_test.py
@@ -1,15 +1,111 @@
 from os import getenv
 import requests
 
-def test_post_headers_body_json():
-    url = getenv("API_URL") or 'http://localhost:8000/'
-    # Additional headers.
-    headers = {'Content-Type': 'application/json' }
+url = getenv("API_URL") or 'http://localhost:8000/'
 
-    # Body
-    # convert dict to json by json.dumps() for body data.
+
+def test_root():
     resp = requests.get(url)
     # Validate response headers and body contents, e.g. status code.
     assert resp.status_code == 200
     resp_body = resp.json()
     assert resp_body['message'] == "Hello World!"
+
+
+def test_create_initiative():
+    # Arrange
+    initiative_name = "Test initiative"
+
+    # Act
+    resp = requests.post(url + "initiatives/", json={"name": initiative_name})
+
+    # Assert
+    assert resp.status_code == 200
+
+    resp_body = resp.json()
+    assert resp_body['name'] == initiative_name
+    assert resp_body['id'] is not None
+
+
+def test_update_initiative():
+    # Arrange
+    initiative_name = "Test initiative"
+    create_resp = requests.post(url + "initiatives/", json={"name": initiative_name})
+
+    assert create_resp.status_code == 200
+
+    resp_body = create_resp.json()
+    initiative_id = resp_body['id']
+
+    assert resp_body['name'] == initiative_name
+    assert initiative_id is not None
+
+    updated_name = "Test initiative with updated name"
+
+    # Act
+    update_resp = requests.put(url + f"initiatives/{initiative_id}", json={"name": updated_name})
+
+    # Assert
+    resp_body = update_resp.json()
+    updated_initiative_id = resp_body['id']
+
+    assert resp_body['name'] == updated_name
+    assert updated_initiative_id == initiative_id
+
+
+def test_get_initiative():
+    # Arrange
+    initiative_name = "Test initiative"
+    create_resp = requests.post(url + "initiatives/", json={"name": initiative_name})
+
+    assert create_resp.status_code == 200
+
+    resp_body = create_resp.json()
+    initiative_id = resp_body['id']
+
+    assert resp_body['name'] == initiative_name
+    assert initiative_id is not None
+
+    # Act
+    get_resp = requests.get(url + f"initiatives/{initiative_id}")
+
+    # Assert
+    resp_body = get_resp.json()
+    initiative_id = resp_body['id']
+
+    assert resp_body['name'] == initiative_name
+    assert initiative_id == initiative_id
+
+
+def test_get_initiative_events():
+    # Arrange
+    initiative_name = "Test initiative"
+    create_resp = requests.post(url + "initiatives/", json={"name": initiative_name})
+
+    assert create_resp.status_code == 200
+
+    resp_body = create_resp.json()
+    initiative_id = resp_body['id']
+
+    assert resp_body['name'] == initiative_name
+    assert initiative_id is not None
+
+    updated_name = "Test initiative with updated name"
+
+    update_resp = requests.put(url + f"initiatives/{initiative_id}", json={"name": updated_name})
+
+    resp_body = update_resp.json()
+    updated_initiative_id = resp_body['id']
+
+    assert resp_body['name'] == updated_name
+    assert updated_initiative_id == initiative_id
+
+    # Act
+    get_events_resp = requests.get(url + f"initiatives/{initiative_id}/events")
+
+    # Assert
+    events = get_events_resp.json()
+
+    assert len(events) == 2
+    assert events[0]["name"] == "planning.initiative.initiative-defined"
+    assert events[1]["name"] == "planning.initiative.initiative-modified"
--- api/api/main.py
+++ api/api/main.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field, asdict
+from dataclasses import dataclass, field, asdict, replace
 from datetime import datetime
 from typing import Optional, List, Dict, Any
 from uuid import UUID, uuid4
@@ -6,12 +6,12 @@ from uuid import UUID, uuid4
 import uvicorn
 from fastapi import FastAPI, HTTPException
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 @dataclass()
 class Event:  # TODO: Move this to it's own module
-    body: Any
+    body: Any = None
     name: Optional[str] = None  # TODO: Should not have to set to none, but failing if I don't.
     created: datetime = field(default_factory=datetime.utcnow)
     effective_date: datetime = field(default_factory=datetime.utcnow)
@@ -32,9 +32,21 @@ class InitiativeDefinedBody:  # TODO: Might be better to make it a namedtuple
     name: str
 
 
+@dataclass()
 class InitiativeDefined(Event):
+    body: InitiativeDefinedBody = None
     name: Optional[str] = "planning.initiative.initiative-defined"
-    body: InitiativeDefinedBody
+
+
+@dataclass()
+class InitiativeModifiedBody:  # TODO: Need to figure out how to set only modified properties
+    name: Optional[str]
+
+
+@dataclass()
+class InitiativeModified(Event):
+    body: InitiativeDefinedBody = None
+    name: Optional[str] = "planning.initiative.initiative-modified"
 
 
 class InitiativeRequest(BaseModel):
@@ -44,6 +56,15 @@ class InitiativeRequest(BaseModel):
 class InitiativeResponse(BaseModel):
     id: UUID
     name: str
+
+
+class EventResponse(BaseModel):
+    body: Any
+    name: Optional[str] = None  # TODO: Should not have to set to none, but failing if I don't.
+    created: datetime = Field(default_factory=datetime.utcnow)
+    effective_date: datetime = Field(default_factory=datetime.utcnow)
+    user_id: Optional[UUID] = None
+    aggregate_id: Optional[UUID] = None
 
 
 app = FastAPI()
@@ -65,6 +86,23 @@ async def find_initiative(initiative_id: UUID) -> Initiative:
     return repo.get(initiative_id)
 
 
+async def modify_initiative(initiative_id: UUID, initiative_request: InitiativeRequest):
+    initiative = repo.get(initiative_id)
+
+    if initiative is None:
+        return None
+
+    changes = initiative_request.dict(exclude_unset=True)
+    updated_initiative = replace(initiative, **changes)
+
+    updated_initiative.event_stream.append(InitiativeModified(aggregate_id=initiative.id,
+                                                              body=InitiativeModifiedBody(**changes)))
+
+    repo[updated_initiative.id] = updated_initiative
+
+    return updated_initiative
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
@@ -73,6 +111,19 @@ async def root():
 @app.post("/initiatives")
 async def create_initiative(initiative_request: InitiativeRequest):
     initiative = await define_initiative(initiative_request)
+
+    response = InitiativeResponse(id=initiative.id,
+                                  name=initiative.name)
+
+    return response
+
+
+@app.put("/initiatives/{initiative_id}")
+async def update_initiative(initiative_id: UUID, initiative_request: InitiativeRequest):
+    initiative = await modify_initiative(initiative_id, initiative_request)
+
+    if initiative is None:
+        raise HTTPException(status_code=404, detail="Initiative not found")
 
     response = InitiativeResponse(id=initiative.id,
                                   name=initiative.name)
@@ -89,6 +140,18 @@ async def get_initiative(initiative_id: UUID):
 
     return InitiativeResponse(id=initiative.id,
                               name=initiative.name)
+
+
+@app.get("/initiatives/{initiative_id}/events", response_model=List[EventResponse])
+async def get_initiative_events(initiative_id: UUID):
+    initiative = await find_initiative(initiative_id)
+
+    if initiative is None:
+        raise HTTPException(status_code=404, detail="Initiative not found")
+
+    stream = [EventResponse(**asdict(event)) for event in initiative.event_stream]
+
+    return stream
 
 
 if __name__ == "__main__":
